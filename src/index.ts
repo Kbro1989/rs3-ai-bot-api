8e8a61a650cd4bfc6a8669e3b862cbf2d76d2422
Kbro1989 <149967879+Kbro1989@users.noreply.github.com>
2025-10-18 18:59:09 -0500

    Update API title and description in index.ts

--- src/index.ts
+++ src/index.ts
@@ -33,9 +33,9 @@ const openapi = fromHono(app, {
 	docs_url: "/",
 	schema: {
 		info: {
-			title: "My Awesome API",
+			title: "My Project API",
 			version: "2.0.0",
-			description: "This is the documentation for my awesome API.",
+			description: "This is the documentation for my project API.",
 		},
 	},
 });
